a1c0e483c534fe4088421cceeae184a8c165c324
Sebastian van der Est <sebastian.vanderest@gmail.com>
2019-02-14 20:41:35 -0500

    Initial Commit

---
+++ Lab2Library/SalesCommission.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Library
+{
+    public class SalesCommission
+    {
+        public double Sales;
+        public double Quota;       
+
+        public double PercentOfQuota
+        {
+            get
+            {
+                if (Sales < 0 || Quota < 0)
+                    throw new Exception();
+                return Math.Round(Sales / Quota * 100, 2);
+            }
+        }
+
+        public double CommissionRate
+        {
+            get
+            {
+                if (PercentOfQuota < 95)
+                    return 0;
+                else if (PercentOfQuota < 100)
+                    return 3;
+                else if (PercentOfQuota < 120)
+                    return 0.25 * PercentOfQuota - 19;
+                else if (PercentOfQuota < 150)
+                    return 0.35*PercentOfQuota - 31;
+                else
+                    return 0.45 * PercentOfQuota - 46;
+            }
+        }
+
+        public double Commission
+        {
+            get
+            {
+                return Sales * CommissionRate;
+            }
+        }
+
+        public SalesCommission(double sales, double quota)
+        {
+            Sales = sales;
+            Quota = quota;
+        }
+    }
+}
---
+++ Lab2Test/SalesCommissionTests.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab2Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]        
+        public void TestMethod1()
+        {
+            Math.Log(-1);
+
+            Assert.Fail();
+        }
+    }
+}
